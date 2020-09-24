7f1e27619d06fc55e2e92bae84aed817d9ca1508
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-24 13:01:16 +0300

    done

---
+++ story.js
@@ -0,0 +1,4 @@
+let NAME='Mshary'
+let AGE_PLUS_33=25
+let HOBBY='Meditation'
+console.log(`Mr./Mrs. ${NAME}, in 33 years, you will be ${AGE_PLUS_33+33} years old. By then you will have decades of ${HOBBY} practice!`)
