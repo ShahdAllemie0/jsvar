e8aaa9ee42a9ed8d7dfd5cd35008fcb2e857b4f8
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-24 14:04:29 +0300

    complete second task

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const first=10
+const sec=6
+const op='*'
+
+
+if(op=='+'){
+  console.log(`${first} + ${sec} = ${first+sec}`)
+}
+else if(op=='-'){
+  console.log(`${first} - ${sec} = ${first-sec}`)
+}
+else if(op=='*'){
+  console.log(`${first} * ${sec} = ${first*sec}`)
+}
+else if(op=='/'){
+  console.log(`${first} / ${sec} = ${first/sec}`)
+}
